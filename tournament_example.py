afe682163748414d0510a90a4640a0dd3c0710f7
Shawn Varghese <svarghs8531@gmail.com>
2024-05-15 21:04:21 -0400

    hopefully running correctly

--- tournament_example.py
+++ tournament_example.py
@@ -7,7 +7,26 @@ greedy1 = prisoners.Greedy("greedy1")
 altruist1 = prisoners.Altruist("altruist1")
 random1 = prisoners.Random("random1")
 titfortat1 = prisoners.TitForTat("titForTat1")
-prisoners_list = [cheater1, cooperator1, greedy1, altruist1, random1, titfortat1]
+cheater2 = prisoners.Cheater("cheater2")
+cooperator2 = prisoners.Cooperator("cooperator2")
+greedy2 = prisoners.Greedy("greedy2")
+altruist2 = prisoners.Altruist("altruist2")
+random2 = prisoners.Random("random2")
+titfortat2 = prisoners.TitForTat("titForTat2")
+prisoners_list = [
+    cheater1,
+    cooperator1,
+    greedy1,
+    altruist1,
+    random1,
+    titfortat1,
+    cheater2,
+    cooperator2,
+    greedy2,
+    altruist2,
+    random2,
+    titfortat2,
+]
 
 tourney = tournament.Tournament(prisoners_list)
 results = tourney.play(100)
